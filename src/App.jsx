9b172afef5e92ed2ab4cdcb46d014fb24376fcd8
amanpages <151524274+amanpages@users.noreply.github.com>
2024-06-15 14:31:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Md Aman",
+  title: "Full Stack Web Developer",
+  email: "mdamana2@gmail.com",
+  gitHub: "https://github.com/amanpages",
+  instagram: "https://www.instagram.com/fit_ambivert/",
+  linkedIn: "https://www.linkedin.com/in/md-aman-1282b4169",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "#",
+  youTube: "#",
 };
 
 const primaryColor = "#4E567E";
