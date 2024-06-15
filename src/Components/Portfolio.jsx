324ea390a5ce71e45fea11176523190a795e4962
amanpages <151524274+amanpages@users.noreply.github.com>
2024-06-15 14:30:36 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "E-Commerce application buuilt in React(NewBee)",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A full stack ecommerce application that handles products and cart functionalities. This also includes the admin control panel.",
+    url: "https://github.com/amanpages/NewBee-MERN-",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Social",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This a a Social media application with all features and database handling.",
+    url: "https://github.com/amanpages/Story",
   },
   {
-    title: "My Resume Site",
+    title: "My Portfolio",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A complete portfolio of me with sanity as backend an React as frontend",
+    url: "mdaman-portfolio.netlify.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Frontend (E-Commerce)",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is the frontend of an E-commerce application made with HTML, CSS, and JAVASCRIPT",
+    url: "e-commere-nu.vercel.app",
   },
 ];
 
